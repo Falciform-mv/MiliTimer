a69487d21db9295b8bb0dfa0dd3495791f9d5ec4
Mike Veneziale <falciformdesign@gmail.com>
2022-03-02 21:12:48 -0600

    added military and standard time

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 
 
-let barreldragon = function() {
-  window.alert("hello");
+let showMilitaryTime = function() {
+  let today = new Date();
+  let time = today.getHours() + ":" + today.getMinutes();
+  return time;
 
 } 
+showMilitaryTime();
 
-barreldragon();
+// conversion to standard time
+let showStandardTime = function() {
+  let militaryTime = showMilitaryTime();
+  let militaryArray = militaryTime.split(":");
+  let militaryHour = parseInt(militaryArray[0]);
+  militaryHour = militaryHour - 12;
+  let standardTime = militaryHour + ":" + militaryArray[1];
+  return standardTime;
+}
+showStandardTime();
 
+
+document.querySelector("#timeDisplay").innerHTML = "The military time is now " + showMilitaryTime();
+
+document.querySelector("#standardTimeDisplay").innerHTML = "The standard time is now " + showStandardTime();
