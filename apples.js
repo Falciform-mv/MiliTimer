d09347855b855055914459eea9969131d991e562
Mike Veneziale <falciformdesign@gmail.com>
2022-04-10 21:07:16 -0500

    working apple counter

--- apples.js
+++ apples.js
@@ -4,11 +4,19 @@ const t = 10;
 const b = 12;
 const apples = [2, 3, -4];
 const oranges = [-3, 2];
+let appleCount = 0;
 
 
 let countApplesandOranges = function(a, s, t, b, apples, oranges) {
   console.log("this is within the function")
   
+  for (i = 0; i < apples.length; i++) {
+    if (a + apples[i] >= s && a + apples[i] <= t) {
+      appleCount ++;
+      console.log('this is apple count ' + appleCount)
+
+    }
+  }
 
 
 }
